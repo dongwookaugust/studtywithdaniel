804350defce785a2da72a05a31f81669b5f5cf22
august <donsw@naver.com>
2024-12-03 17:30:45 +0900

    change git.js

--- js/git.js
+++ js/git.js
@@ -1,1 +1,6 @@
-2;
+//clone - cloning the entire history of the remote repository to your local machine
+//commit -unit that records the current state of a project in a repository
+//push & pull - Used to synchronize changes between local and remote repositories
+//merge - Consolidate changes from two or more branches into one
+//resolve conflict  - occurs when the same part of the same file is modified differently in two branches.
+//branchion -
