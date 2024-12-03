faa61b31dd305d5b4f0243b0a9bcd65cfd67ae74
august <donsw@naver.com>
2024-12-03 16:26:22 +0900

    new file:   js/array.js

---
+++ js/array.js
@@ -0,0 +1,51 @@
+//Array is an ordered set of data(list like object)
+
+let foods = ["chicken", "pizza", "kimbab"];
+//add elemnet to end of array
+foods.push("rice");
+console.log("push=", foods);
+
+//delete element to end of array
+foods.pop();
+console.log("\npop=", foods);
+
+//add element to beginning of array
+foods.shift();
+console.log("\nshift=", foods);
+
+//delete element to beginning of array
+foods.unshift("hamburger");
+console.log("\nunshift=", foods);
+
+//delet a specified number of elements starting from a given index.
+foods.splice(1, 2);
+console.log("\nsplice=", foods);
+
+//add two element at index 1
+foods.splice(1, 0, "coffee", "cake");
+console.log("\nAdd using splice=", foods);
+
+//add sushi after delete index 1
+foods.splice(1, 1, "sushi");
+console.log("\nChange using splice=", foods);
+
+let includeFoods = foods.includes("hamburger");
+console.log("\nUse include =", includeFoods);
+
+//return the index of a specific element..
+let foodIndexOF = foods.indexOf("sushi");
+console.log("\nUse index of=", foodIndexOF);
+
+//make new array copy from foods
+let newFoods = foods.slice(0, 2);
+console.log("\nUse slice=", newFoods);
+
+foods.forEach((foods) => console.log("\nforEach=", foods));
+
+//make a new array by transforming each element of the array
+let upperFoods = foods.map((foods) => foods.toUpperCase());
+console.log("\nmap=", upperFoods);
+
+//array consisting only of elements that meet the conditions
+let filterFoods = foods.filter((foods) => foods.length > 5);
+console.log("\nfilter=", filterFoods);
