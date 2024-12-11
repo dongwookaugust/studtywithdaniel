fab59830ef510f96b82c52c77cc982bf64ea513f
august <donsw@naver.com>
2024-12-11 23:51:02 +0900

    update case file

--- js/case.js
+++ js/case.js
@@ -2,11 +2,13 @@
 //단어가 대문자로 시작하며, 구분자 없음(Ex.MyVariableName)
 //Each word start capital letter with no seperator
 //use to C#, Java,TypeScript
+// class MyClass
 
 //Camel Case
 //첫번째 단어는 소문자로 시작하고 이후에 단어는 대문자로 시작함. 구분자 없음(Ex.myVariableName)
 //First word start lowercase and subsequent word start capital letter with no seperator
 //JavaScript, Java , C#, React
+// myVariable
 
 //Kebab Case
 //all letter are in lower case and seperate by -(hyphens)
@@ -22,3 +24,6 @@
 //모든 단어가 대문자, 단어가_으로 구분(Ex.MY_VARIABLE_NAME)
 //all letter are in uppercase and seperate by _(underscore)
 //Often used for constants and configuration values that should not change throughout the program.
+const MAX_TRY = 5;
+
+const myVariable = MAX_TRY;
