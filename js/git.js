0fe0e6823b49d47d8ef23608738437f54e9432d9
august <donsw@naver.com>
2024-12-13 23:49:33 +0900

    fix a conflict

--- js/git.js
+++ js/git.js
@@ -1,13 +1,13 @@
 // source control management
 // pull request/merge request - same thing
 
-//git - manages the change history of source code
+//git - manages the change history of source code//test
 //github - platform that makes collaboration with team members easier
 
 //clone - Cloning the entire history of the remote repository to your local machine (copy of the repository)
 //commit - Records the current state of a project in a repository (change(or save) repository). you create a stamp. or  a save point
 //push - Uploads local repository changes to a remote repository (uploaded repository)
-//pull - Downloads from a remote repository into the local repository. (downloaded repository)
+//pull - Downloads from a remote repository into the local repository. (downloaded repository). equal to fetch + merge
 //merge - Consolidate changes from two or more branches into one
 //branching - Create new branch for develop features and fixed independently from the main codebase (new save file)
 //resolve conflict  - i also want to change this line. occurs when the same part of the same file is modified differently in two branches. test change here
@@ -27,3 +27,6 @@
 // git checkout test2 ===> switch to the test2 branch
 // git pull origin test2 ===> fetch and merge updates from the remote repository's test2 branch
 // git merge origin/test1 ====> merge fetched changes from remote test2 branch into local test2
+
+// git stash and also git stash pop
+// git rebase - what is difference with git merge
